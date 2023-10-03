a5e2d0144dbf9e847ec43bc0d4dc858ce8338270
Peter Levinsky <pele@zealand.dk>
2023-10-03 14:09:52 +0200

    Add project files.

---
+++ SimpleAritmetikSolution/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Simple Aritmetik");
+
+//Erklæring af variable:
+int i = 12;
+int j = 5;
+double x = 12.0;
+double y = 5.0;
+Console.WriteLine("i = " + i);
+Console.WriteLine("j = " + j);
+Console.WriteLine("x = " + x);
+Console.WriteLine("y = " + y);
+Console.WriteLine();
+
+//udregn og udskriv (Console.WriteLine(…)) følgende udtryk
+Console.WriteLine("i + j = " + (i + j));
+Console.WriteLine("i – j = " + (i - j));
+Console.WriteLine("i / j = " + (i / j));
+Console.WriteLine("i * j = " + (i * j));
+Console.WriteLine("i % j = " + (i % j));
+
+Console.WriteLine("x + y = " + (x + y));
+Console.WriteLine("x - y = " + (x - y));
+Console.WriteLine("x / y = " + (x / y));
+Console.WriteLine("x * y = " + (x * y));
+Console.WriteLine("x % y = " + (x % y));
+
+Console.WriteLine("i / 0 = " + (i / 0));
+Console.WriteLine("x / 0 = " + (x / 0));
